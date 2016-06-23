bd60dbf3f9884d2180de407b0734fccd04a8f722
Isis Gomez <igomez@byr.local>
2016-06-23 12:46:26 -0400

    Configuracion e Inicio de Definicion de Web API para el proyecto

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # MappingCommerce
 Repositorio para proyecto de Aseguramiento de Calidad de Software basado en manejo de ubicación de comercios
+
+
+#### Links de Ayuda
+Building a RestFUL API NodeJS and Express 4
+https://scotch.io/tutorials/build-a-restful-api-using-node-and-express-4
---
+++ server.js
@@ -0,0 +1,28 @@
+// Paquetes que necesitaremos
+var express     = require('express');
+var app         = require();
+var bodyParser  = require('body-parser');
+
+// Configuración de Body Parser
+// Permitirá obtener data from POST
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// Configuración del Puerto de la Aplicación
+var port = process.env.PORT || 8030;
+
+// Configuración del Router de la Aplicación
+var router = express.Router();
+
+// Prueba de Funcionamiento de Router
+router.get('/', function (req, res) {
+    res.json({message: 'Bienvenido a Nuestra API'});
+});
+
+// Registro de Nuestro Router
+// Todos los routes seran prefijados con MappingCommerceAPI
+app.use('MappingCommerceAPI', router);
+
+// Inicio de Ejecución de nuestro Servidor
+app.listen(port);
+console.log('Magic happens on port ' + port);
